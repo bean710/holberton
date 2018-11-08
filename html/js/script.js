9dcee40f07330e6070a416d915ee7f5abb1d7994
Ben Keener <keener4christ@gmail.com>
2018-11-07 19:36:09 -0500

    Various changes

--- html/js/script.js
+++ html/js/script.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event){
-  var cat = document.getElementById("smart_thumbnail");
+	var cat = document.getElementById("smart_thumbnail");
 
-  cat.addEventListener("click", function() {
-    if (cat.classList.contains("small")) {
-      cat.className = "";
-    } else {
-      cat.className = "small";
-    }
-  });
+	cat.addEventListener("click", function() {
+		if (cat.classList.contains("small")) {
+			cat.className = "";
+		} else {
+			cat.className = "small";
+		}
+	});
+
+	var pics = getElementByClass("article-pic");
+
+	for (var img in pics) {
+		img.addEventListener("click", function() {
+			
+		})
+	}
 });
